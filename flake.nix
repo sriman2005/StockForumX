9dc367ff28e551657b0fac4d790fda11643ffb78
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2026-01-31 14:39:41 +0530

    fix(ci): fix syntax error in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description: "StockForumX Development Environment";
+  description = "StockForumX Development Environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
